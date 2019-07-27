644179a2229625d61889d01f78c1e9d390edce81
fahim <fahim@umail.com>
2019-07-27 11:23:10 +0600

    added data sql for populating table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Fahim Feroje Al Jami
+ * Created: Jul 26, 2019
+ */
+-- INSERT INTO location (id, name) VALUES (1, 'Dhaka');
+-- INSERT INTO location (id, name) VALUES (2, 'Khulna');
+-- INSERT INTO location (id, name) VALUES (3, 'Rajshahi');
+-- INSERT INTO location (id, name) VALUES (4, 'Chittagong');
+-- INSERT INTO location (id, name) VALUES (5, 'Sylhet');
+
+
+REPLACE INTO `role` VALUES (1,'ADMIN');
